73c1c86fa9da534a3f6f8a86f7393a7cda91273e
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-08 12:27:23 +0530

    done

--- backend/src/mail.js
+++ backend/src/mail.js
@@ -1,4 +1,7 @@
+import dns from "node:dns";
 import nodemailer from "nodemailer";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
