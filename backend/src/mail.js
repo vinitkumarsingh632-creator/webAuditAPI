01b54ac15e38939e427869dbb3ed232c71b6eda0
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-08 12:30:07 +0530

    done

--- backend/src/mail.js
+++ backend/src/mail.js
@@ -3,6 +3,15 @@ import nodemailer from "nodemailer";
 
 dns.setDefaultResultOrder("ipv4first");
 
+dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
+  if (err) {
+    console.error("DNS ERROR:", err);
+    return;
+  }
+
+  console.log("GMAIL DNS:", addresses);
+});
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
