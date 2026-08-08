fb0b0636f7e1fb9670fc436ae712ea8e0e23dcc8
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-08 12:15:53 +0530

    mail

--- backend/src/mail.js
+++ backend/src/mail.js
@@ -15,4 +15,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
+
 export default transporter;
