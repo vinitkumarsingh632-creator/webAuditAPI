38c225d5b9770b243565eedcc9c5fb48f9bddc0c
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-08 11:58:49 +0530

    mail

--- backend/src/mail.js
+++ backend/src/mail.js
@@ -1,15 +1,20 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
-const result = dotenv.config({
-    path:"./.env.mail"
-})
+import nodemailer from "nodemailer";
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
+  pool: true,
+  maxConnections: 5,
+  maxMessages: 100,
+
+  family: 4,
+
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.APP_PASSWORD
-  }
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
-
-export default transporter
+export default transporter;
